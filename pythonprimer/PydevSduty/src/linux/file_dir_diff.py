d356f57327c5e348d2db9fb70823cc34c1ab6e21
slieer@gmail.com <slieer@gmail.com>
2014-05-11 09:20:34 -0500

    1. config file 2. file diff test.

---
+++ pythonprimer/PydevSduty/src/linux/file_dir_diff.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Created on May 11, 2014
+
+@author: root
+'''
+import os
+
+centos_dvd_dir = '/opt/os/CentOS-6.5-x86_64-bin-DVD1/Packages/'
+u_dir = '/media/CENTOS_6.5_/Packages'
+
+'''
+dve_dir = open(centos_dvd_dir, mode='r')
+u_dir = open(u_dir, mode='r')
+
+IOError: [Errno 21] Is a directory:
+'''
+
+dvd_list = os.listdir(centos_dvd_dir)
+u_list = os.listdir(u_dir)
+
+print "---dif---"
+for x in dvd_list:
+    flag = False
+    for y in u_list:
+        if x == y :
+            flag = True
+            continue
+    if(not flag) :
+        print x
