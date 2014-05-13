95d2ae4d89d78ebea790f4adadbbfa1d079f2434
slieer@gmail.com <slieer@gmail.com>
2014-05-13 09:46:23 -0500

    hash test.

---
+++ pythonprimer/PydevSduty/src/std/hash.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Created on May 11, 2014
+
+@author: root
+'''
+
+import os
+import hashlib
+
+src = "teststring"
+val = hashlib.md5(src).hexdigest();
+print(val)
+
+m0 = hashlib.md5()
+m0.update(src)
+m0.update('hi')
+m0.update('slieer')
+val = m0.hexdigest().upper()
+print val
+
+"""file test"""
+def getFileMd5(fileName):
+    if not os.path.isfile(fileName) :
+        return
+    myHash = hashlib.md5();
+    f = file(fileName, 'rb')
+    
+    while True :
+        b = f.read(8096);
+        if not b :
+            break
+        myHash.update(b)
+    f.close()
+    return myHash.hexdigest().upper()
+
+print getFileMd5('./hash.py')
+    
