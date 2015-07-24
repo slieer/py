48e8d20ff557ae42d9162f3060e88bbf0cf8333f
slieer <slieer@gmail.com>
2015-07-24 14:51:04 +0800

    func arg test.

--- pythonprimer/pydevstudy/src/simple/function_arg.py
+++ pythonprimer/pydevstudy/src/simple/function_arg.py
@@ -3,16 +3,17 @@
 import glob
 import os
 
-def function(arg1="hello", arg2="slieer"):
+def func_arg_1(arg1="hello", arg2="slieer"):
     print "arg1:%s" %arg1
     print "arg2:%s" %(arg2)
 
 
-def foo(*args):
+def fun_arg_2(*args):
     numArgs = len(args)
     print "Number of arguments:%s" %numArgs
     for i, x in enumerate(args):
         print "Argument %s is %s" %(i,x)
+
 def findFiles():
     files = glob.glob('*.py')
     for x in files:
@@ -20,6 +21,7 @@ def findFiles():
         print real
 
 if __name__ == '__main__':
-    function()
-    foo('x','y','z')
+    func_arg_1()
+    func_arg_1(arg1='xx', arg2='xxxx')
+    fun_arg_2('x','y','z')
     findFiles()
