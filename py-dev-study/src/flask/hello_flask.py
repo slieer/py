02e4d9ee52f8991889c09bc37b600afbe3405bf5
slieer <slieer@gmail.com>
2020-07-03 07:08:55 +0800

    flask learn.

--- py-dev-study/src/flask/hello_flask.py
+++ py-dev-study/src/flask/hello_flask.py
@@ -3,6 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Index Page!'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
 #run:
 # env FLASK_APP=hello_flask.py flask run
+
+#export FLASK_DEBUG=1
+#export FLASK_APP=flask/hello_flask.py 
+#flask run --host=0.0.0.0
