f01804dce13705be23948e82fbd0fc9eb17183f7
zhaixiaobin <zhaixiaobin@skyworth.com>
2020-07-03 10:02:32 +0800

    python 类型提示

--- py-dev-study/src/simple/data_type.py
+++ py-dev-study/src/simple/data_type.py
@@ -19,3 +19,28 @@ print(type(ar) is list)
 print(isinstance(ar, list))
 
 print(dir(types))
+
+#类型提示。但这只是一种注解，并不能在运行的时候去检查对与错。
+def add(x:int,y:int)->int:
+    return x+y
+
+print(add(10,12))
+print(add('hello','world')) # 无法做到检查
+
+x = 1
+y = 2
+(y, x) = (x, y)
+#print("x=" + x  + ", y=" + y)
+
+# fib.py
+from typing import Iterator
+ 
+ 
+def fib(n: int) -> Iterator[int]:
+  a, b = 0, 1
+  while a < n:
+    yield a
+    a, b = b, a + b
+ 
+i = fib(3)
+print(next(i))
