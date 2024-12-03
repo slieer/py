aaabed7535a7c35553b70b7b79db8a61a56400bd
zhai <slieer@gmail.com>
2024-12-03 21:54:28 +0800

    dir structure modified.

--- py-dev-study/src/flask/hello_template.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-
-from flask import Flask, escape
-app = Flask(__name__)
-
-@app.route('/hello-tpl/')
-@app.route('/hello-tpl/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
