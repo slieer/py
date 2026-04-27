8907b0b2e0ed6c07f1714b1528209e855082544c
zhai <491089448@qq.com>
2026-04-27 22:17:05 +0800

    py http client 测试

--- py-dev-study/src/ch00_simple/05_function_built_in_func_date.py
+++ py-dev-study/src/ch00_simple/05_function_built_in_func_date.py
@@ -1,51 +1,56 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Created on 2012-10-7
 
 @author: me
-'''
+"""
 from ch00_simple.ch00_01_hello_world_v1 import Basic
 
 b = Basic()
 
-from datetime import datetime     
+from datetime import datetime
 from datetime import date
 import time
 
-def dateTest(): 
-        print('date.max:', date.max)
-        print('date.min:', date.min)
-        today = date.today()
-        print('date.today():', today)
-        print('date.fromtimestamp():', date.fromtimestamp(time.time()))
+
+def dateTest():
+    print("date.max: %s" % (date.max))
+    print("date.min:", date.min)
+    today = date.today()
+    print("date.today():", today)
+    print("date.fromtimestamp():", date.fromtimestamp(time.time()))
+
 
 def dateTest1():
-        now = date(2012, 10, 0o7)
-        tomorrow = now.replace(day = 0o7)
-        print('now =%s, tomorrow: %s' %(now,tomorrow))
-        print('timetuple():', now.timetuple())
-        print('weekday():', now.weekday())
-        print('isoweekday():', now.isoweekday())
-        print('isocalendar():', now.isocalendar())
-        print('isoformat():', now.isoformat())
+    now = date(2012, 10, 0o7)
+    tomorrow = now.replace(day=0o7)
+    print("now =%s, tomorrow: %s" % (now, tomorrow))
+    print("timetuple():", now.timetuple())
+    print("weekday():", now.weekday())
+    print("isoweekday():", now.isoweekday())
+    print("isocalendar():", now.isocalendar())
+    print("isoformat():", now.isoformat())
+
 
 def dateTest2():
-        print('datetime.resolution:', datetime.resolution)
-        print('today():', datetime.today())
-        print('now():', datetime.now())
-        print('utcnow():', datetime.utcnow())
-        print('fromtimestamp(tmstmp):', datetime.fromtimestamp(time.time()))
-        print('utcfromtimestamp(tmstmp):', datetime.utcfromtimestamp(time.time()))
-        print('timetuple:', datetime.now().timetuple())
-        print('utctimetuple:',datetime.now().utctimetuple())
-        print('toordinal', datetime.now().toordinal())
-        print('isocalendar:',datetime.now().isocalendar())
-        print('ctime:', datetime.now().ctime())
-        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S %f'))
+    print("datetime.resolution:", datetime.resolution)
+    print("today():", datetime.today())
+    print("now():", datetime.now())
+    print("utcnow():", datetime.utcnow())
+    print("fromtimestamp(tmstmp):", datetime.fromtimestamp(time.time()))
+    print("utcfromtimestamp(tmstmp):", datetime.utcfromtimestamp(time.time()))
+    print("timetuple:", datetime.now().timetuple())
+    print("utctimetuple:", datetime.now().utctimetuple())
+    print("toordinal", datetime.now().toordinal())
+    print("isocalendar:", datetime.now().isocalendar())
+    print("ctime:", datetime.now().ctime())
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S %f"))
+
 
 def dateTest3():
-        pass
-if __name__ == '__main__' :
-        #dateTest1()        
-        dateTest2()        
+    pass
 
+
+if __name__ == "__main__":
+    # dateTest1()
+    dateTest2()
